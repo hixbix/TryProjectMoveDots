d1aa3a572f975562de82df088904b7e8e1d640c7
hixbix <lior5963@gmail.com>
2023-12-27 21:16:10 +0200

    Another one bites the dust!

--- src/Mission2/Main.java
+++ src/Mission2/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] arg){
         new DrawingJFrame();
         System.out.println("This is my change");
+        System.out.println("This is another update for the my legacy");
     }
 }
